8b31f1db185e4a49b4dfdd8e6d724948b5054d07
will-o-the-wisp <will-o-the-wisp@blesschoice.blesschoice>
2014-05-21 15:05:06 +0300

    category project CRUD created

---
+++ src/WebKate/Bundle/TestTaskBundle/Form/ProjectType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WebKate\Bundle\TestTaskBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProjectType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('createdAt')
+            ->add('customer')
+            ->add('category')
+            ->add('executors')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'WebKate\Bundle\TestTaskBundle\Entity\Project'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'webkate_bundle_testtaskbundle_project';
+    }
+}
