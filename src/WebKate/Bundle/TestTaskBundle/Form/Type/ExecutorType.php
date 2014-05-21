d09dc16ac47db6927627320ca88e545f520020c2
will-o-the-wisp <will-o-the-wisp@blesschoice.blesschoice>
2014-05-21 14:50:06 +0300

    added Executor Form Type

---
+++ src/WebKate/Bundle/TestTaskBundle/Form/Type/ExecutorType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace WebKate\Bundle\TestTaskBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ExecutorType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('secondName')
+            ->add('firstName')
+            ->add('patronymic')
+            ->add('birthday')
+            ->add('careerBeggining')
+            ->add('email')
+            ->add('phoneNumber')
+            ->add('address')
+            ->add('technologyUsed')
+            ->add('projects')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'WebKate\Bundle\TestTaskBundle\Entity\Executor'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'webkate_bundle_testtaskbundle_executor';
+    }
+}
