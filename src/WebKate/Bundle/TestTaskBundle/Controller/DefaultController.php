f2b528443dfe04fe49d2cdc3dad9087c28803169
will-o-the-wisp <will-o-the-wisp@blesschoice.blesschoice>
2014-05-28 16:17:35 +0300

    added createExecutor Action

--- src/WebKate/Bundle/TestTaskBundle/Controller/DefaultController.php
+++ src/WebKate/Bundle/TestTaskBundle/Controller/DefaultController.php
@@ -3,9 +3,31 @@
 namespace WebKate\Bundle\TestTaskBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use WebKate\Bundle\TestTaskBundle\Entity\Executor;
+use WebKate\Bundle\TestTaskBundle\Form\Type\ExecutorType;
 
 class DefaultController extends Controller
 {
+
+    public function createExecutorAction(Request $request)
+    {
+        $executor = new Executor();
+        $form = $this->createForm(new ExecutorType(), $executor);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $om = $this->getDoctrine()->getManager();
+            $om->persist($form->getData());
+            $om->flush();
+
+            return $this->redirect($this->generateUrl('_homepage'));
+        }
+
+        return $this->render('WebKateTestTaskBundle:Default:createExecutor.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
     public function indexAction()
     {
         $projects = $this->getDoctrine()
